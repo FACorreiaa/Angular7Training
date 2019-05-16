767d32b8fd5b2769967e07d9343c1ad858fb91ec
Knox316 <fernandocorreia316@gmail.com>
2019-05-16 13:44:03 +0100

    added directives to shopping

---
+++ shoppingApp/src/app/shared/dowpdown.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DowpdownDirective } from './dowpdown.directive';
+
+describe('DowpdownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DowpdownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
